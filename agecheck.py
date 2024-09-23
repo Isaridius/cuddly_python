e6e65a3d1ac6d3355fac58f44773adb7b403a1db
andrewtang <andrew.z.tang@gmail.com>
2024-09-23 09:02:29 -0400

    Can we drink?

---
+++ agecheck.py
@@ -0,0 +1,13 @@
+print("What is your name? ")
+name = input()
+print("What is your last name? ")
+lastname = input()
+print("What is your birth year? ")
+year = int(input())
+
+age=2024-year
+
+if age>=19:
+    print(f"Hello {name} {lastname}, you are {age}, and are able to drink in Canada.")
+else:
+    print(f"Hello {name} {lastname}, you are {age}, and are not able to drink in Canada.")
